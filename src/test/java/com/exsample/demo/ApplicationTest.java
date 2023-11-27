5e313c62dc5306c24a703f0028552ee0cd9c3565
lyffange <lyffange@163.com>
2023-11-27 15:23:01 +0800

    新建springboot项目启动类

---
+++ src/test/java/com/exsample/demo/ApplicationTest.java
@@ -0,0 +1,12 @@
+package com.exsample.demo;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTest {
+    @Test
+    public void test() {
+        System.out.println("Spring Boot Test!");
+    }
+}
